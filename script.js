5e9f089b362e3d23fdc00bd05de9bbae87e75e91
EdGab <Eduka.gabrielyan@gmail.com>
2023-11-21 08:27:24 +0100

    Aufgabe 12 Korrigiert

--- script.js
+++ script.js
@@ -182,9 +182,11 @@ export function aufgabe07(args) {
 export function aufgabe12 (args) {
   const input = args
   const result = []
+  let pos = -1
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
+    pos = pos + 2
     if (currentElement === "e") {
       return i
     }
